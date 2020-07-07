81623892e9e1ac30043571227bb2494e864b78f8
Olly <dninosores@gmail.com>
2020-07-06 18:32:29 -0700

    Fixed namespaces.

--- ConditionalHideAttribute.cs
+++ ConditionalHideAttribute.cs
@@ -5,71 +5,76 @@ using System.Collections;
 //Original version of the ConditionalHideAttribute created by Brecht Lecluyse (www.brechtos.com)
 //Modified by: dninosores
 
-//Known limitation: Cannot find fields of fields if any of the nested fields have the same name (for example, test.test)
-
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property |
-    AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
-public class ConditionalHideAttribute : PropertyAttribute
+namespace dninosores.UnityConditionalHideAttribute
 {
-    public (string field, object comparison)[] conditions;
-    public enum FoldBehavior
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property |
+        AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+    public class ConditionalHideAttribute : PropertyAttribute
     {
-        And, 
-        Or
-    }
-
-    public FoldBehavior foldBehavior;
-
-
-	// Use this for initialization
-    public ConditionalHideAttribute(FoldBehavior foldBehavior, params (string, object)[] conditions)
-    {
-        this.foldBehavior = foldBehavior;
-        this.conditions = conditions;
-    }
-
-    public ConditionalHideAttribute(params (string, object)[] conditions) : this(FoldBehavior.And, conditions)
-    {
-
-    }
-
-    public ConditionalHideAttribute(string field, object comparison)
-    {
-        conditions = new (string, object)[]
+        public (string field, object comparison)[] conditions;
+        public enum FoldBehavior
         {
+            And,
+            Or
+        }
+
+        public FoldBehavior foldBehavior;
+
+
+        // Use this for initialization
+        public ConditionalHideAttribute(FoldBehavior foldBehavior, params (string, object)[] conditions)
+        {
+            this.foldBehavior = foldBehavior;
+            this.conditions = conditions;
+        }
+
+        public ConditionalHideAttribute(params (string, object)[] conditions) : this(FoldBehavior.And, conditions)
+        {
+
+        }
+
+        public ConditionalHideAttribute(string field, object comparison)
+        {
+            conditions = new (string, object)[]
+            {
             (field, comparison)
-        };
-        foldBehavior = FoldBehavior.And;
-    }
-
-    public ConditionalHideAttribute(string[] fields, object[] comparisons, FoldBehavior foldBehavior = FoldBehavior.And)
-    {
-        this.foldBehavior = foldBehavior;
-        if (fields == null) throw new NullReferenceException("Fields[] cannot be null");
-        if (comparisons == null) throw new NullReferenceException("Comparisons[] cannot be null");
-        if (fields.Length != comparisons.Length) throw new ArgumentException("Field and comparison arrays must be same length!");
-
-        conditions = new (string, object)[fields.Length];
-
-        for (int i = 0; i < fields.Length; i++)
-        {
-            conditions[i] = (fields[i], comparisons[i]);
+            };
+            foldBehavior = FoldBehavior.And;
         }
-    }
 
-    public bool Combine(bool left, bool right)
-    {
-        switch (foldBehavior)
+        public ConditionalHideAttribute(string[] fields, object[] comparisons, FoldBehavior foldBehavior = FoldBehavior.And)
         {
-            case (FoldBehavior.And):
-                return left && right;
-            case (FoldBehavior.Or):
-                return left || right;
-            default:
-                throw new NotImplementedException("No case for fold behavior " + foldBehavior);
-        }
-    }
+            this.foldBehavior = foldBehavior;
+            if (fields == null)
+                throw new NullReferenceException("Fields[] cannot be null");
+            if (comparisons == null)
+                throw new NullReferenceException("Comparisons[] cannot be null");
+            if (fields.Length != comparisons.Length)
+                throw new ArgumentException("Field and comparison arrays must be same length!");
 
+            conditions = new (string, object)[fields.Length];
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                conditions[i] = (fields[i], comparisons[i]);
+            }
+        }
+
+        public bool Combine(bool left, bool right)
+        {
+            switch (foldBehavior)
+            {
+                case (FoldBehavior.And):
+                    return left && right;
+                case (FoldBehavior.Or):
+                    return left || right;
+                default:
+                    throw new NotImplementedException("No case for fold behavior " + foldBehavior);
+            }
+        }
+
+    }
 }
 
 
--- Editor/ConditionalHidePropertyDrawer.cs
+++ Editor/ConditionalHidePropertyDrawer.cs
@@ -5,129 +5,133 @@ using System;
 using System.Text.RegularExpressions;
 
 //Original version of the ConditionalHideAttribute created by Brecht Lecluyse (www.brechtos.com)
-//Modified by: -
+//Modified by: dninosores
 
-[CustomPropertyDrawer(typeof(ConditionalHideAttribute))]
-public class ConditionalHidePropertyDrawer : PropertyDrawer
+namespace dninosores.UnityConditionalHideAttribute
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+
+    [CustomPropertyDrawer(typeof(ConditionalHideAttribute))]
+    public class ConditionalHidePropertyDrawer : PropertyDrawer
     {
-        
-        ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
-        bool enabled = GetConditionalHideAttributeResult(condHAtt, property);        
-
-        bool wasEnabled = GUI.enabled;
-        GUI.enabled = enabled;
-        if (enabled)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            EditorGUI.PropertyField(position, property, label, true);
-        }        
 
-        GUI.enabled = wasEnabled;
-    }
+            ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
+            bool enabled = GetConditionalHideAttributeResult(condHAtt, property);
 
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
-        bool enabled = GetConditionalHideAttributeResult(condHAtt, property);
+            bool wasEnabled = GUI.enabled;
+            GUI.enabled = enabled;
+            if (enabled)
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
 
-        if (enabled)
-        {
-            return EditorGUI.GetPropertyHeight(property, label);
-        }
-        else
-        {
-            //The property is not being drawn
-            //We want to undo the spacing added before and after the property
-            return -EditorGUIUtility.standardVerticalSpacing;
-            //return 0.0f;
+            GUI.enabled = wasEnabled;
         }
 
-
-        /*
-        //Get the base height when not expanded
-        var height = base.GetPropertyHeight(property, label);
-
-        // if the property is expanded go through all its children and get their height
-        if (property.isExpanded)
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            var propEnum = property.GetEnumerator();
-            while (propEnum.MoveNext())
-                height += EditorGUI.GetPropertyHeight((SerializedProperty)propEnum.Current, GUIContent.none, true);
-        }
-        return height;*/
-    }
+            ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
+            bool enabled = GetConditionalHideAttributeResult(condHAtt, property);
 
-    private bool GetConditionalHideAttributeResult(ConditionalHideAttribute condHAtt, SerializedProperty property)
-    {
-        bool enabled;
-        switch (condHAtt.foldBehavior)
-        {
-            case (ConditionalHideAttribute.FoldBehavior.And):
-     
-                enabled = true;
-                break;
-            case (ConditionalHideAttribute.FoldBehavior.Or):
-
-                enabled = false;
-                break;
-            default:
-                throw new NotImplementedException("Case not found for FoldBehavior " + condHAtt.foldBehavior);
-        }
+            if (enabled)
+            {
+                return EditorGUI.GetPropertyHeight(property, label);
+            }
+            else
+            {
+                //The property is not being drawn
+                //We want to undo the spacing added before and after the property
+                return -EditorGUIUtility.standardVerticalSpacing;
+                //return 0.0f;
+            }
 
 
-        foreach ((string field, object comp) pair in condHAtt.conditions)
-        {
-            enabled = condHAtt.Combine(enabled, CheckPropertyType(GetFieldFromProperty(property, pair.field), pair.comp));
+            /*
+            //Get the base height when not expanded
+            var height = base.GetPropertyHeight(property, label);
+
+            // if the property is expanded go through all its children and get their height
+            if (property.isExpanded)
+            {
+                var propEnum = property.GetEnumerator();
+                while (propEnum.MoveNext())
+                    height += EditorGUI.GetPropertyHeight((SerializedProperty)propEnum.Current, GUIContent.none, true);
+            }
+            return height;*/
         }
 
-        return enabled;
-    }
-
-
-    private SerializedProperty GetFieldFromProperty(SerializedProperty property, string field)
-    {
-        //Handle primary property
-        SerializedProperty sourcePropertyValue = null;
-        //Get the full relative property path of the sourcefield so we can have nested hiding.Use old method when dealing with arrays
-        if (!property.isArray)
+        private bool GetConditionalHideAttributeResult(ConditionalHideAttribute condHAtt, SerializedProperty property)
         {
-            string propertyPath = property.propertyPath; //returns the property path of the property we want to apply the attribute to
-            Regex replaceSearch = new Regex(property.name + "$");
-            string conditionPath = replaceSearch.Replace(propertyPath, (Match m) => field);
-            sourcePropertyValue = property.serializedObject.FindProperty(conditionPath);
+            bool enabled;
+            switch (condHAtt.foldBehavior)
+            {
+                case (ConditionalHideAttribute.FoldBehavior.And):
 
-            //if the find failed->fall back to the old system
-            if (sourcePropertyValue == null)
+                    enabled = true;
+                    break;
+                case (ConditionalHideAttribute.FoldBehavior.Or):
+
+                    enabled = false;
+                    break;
+                default:
+                    throw new NotImplementedException("Case not found for FoldBehavior " + condHAtt.foldBehavior);
+            }
+
+
+            foreach ((string field, object comp) pair in condHAtt.conditions)
+            {
+                enabled = condHAtt.Combine(enabled, CheckPropertyType(GetFieldFromProperty(property, pair.field), pair.comp));
+            }
+
+            return enabled;
+        }
+
+
+        private SerializedProperty GetFieldFromProperty(SerializedProperty property, string field)
+        {
+            //Handle primary property
+            SerializedProperty sourcePropertyValue = null;
+            //Get the full relative property path of the sourcefield so we can have nested hiding.Use old method when dealing with arrays
+            if (!property.isArray)
+            {
+                string propertyPath = property.propertyPath; //returns the property path of the property we want to apply the attribute to
+                Regex replaceSearch = new Regex(property.name + "$");
+                string conditionPath = replaceSearch.Replace(propertyPath, (Match m) => field);
+                sourcePropertyValue = property.serializedObject.FindProperty(conditionPath);
+
+                //if the find failed->fall back to the old system
+                if (sourcePropertyValue == null)
+                {
+                    //original implementation (doens't work with nested serializedObjects)
+                    sourcePropertyValue = property.serializedObject.FindProperty(field);
+                }
+            }
+            else
             {
                 //original implementation (doens't work with nested serializedObjects)
                 sourcePropertyValue = property.serializedObject.FindProperty(field);
             }
+
+            return sourcePropertyValue;
         }
-        else
+
+
+        private bool CheckPropertyType(SerializedProperty sourcePropertyValue, object comparison)
         {
-            //original implementation (doens't work with nested serializedObjects)
-            sourcePropertyValue = property.serializedObject.FindProperty(field);
-        }
-
-        return sourcePropertyValue;
-    }
-
-
-    private bool CheckPropertyType(SerializedProperty sourcePropertyValue, object comparison)
-    {
-        //Note: add others for custom handling if desired
-        switch (sourcePropertyValue.propertyType)
-        {                
-            case SerializedPropertyType.Boolean:
-                return sourcePropertyValue.boolValue;                
-            case SerializedPropertyType.ObjectReference:
-                return sourcePropertyValue.objectReferenceValue != null;
-            case SerializedPropertyType.Enum:
-                return sourcePropertyValue.enumValueIndex == (int)comparison;
-            default:
-                Debug.LogError("Data type of the property used for conditional hiding [" + sourcePropertyValue.propertyType + "] is currently not supported");
-                return true;
+            //Note: add others for custom handling if desired
+            switch (sourcePropertyValue.propertyType)
+            {
+                case SerializedPropertyType.Boolean:
+                    return sourcePropertyValue.boolValue;
+                case SerializedPropertyType.ObjectReference:
+                    return sourcePropertyValue.objectReferenceValue != null;
+                case SerializedPropertyType.Enum:
+                    return sourcePropertyValue.enumValueIndex == (int)comparison;
+                default:
+                    Debug.LogError("Data type of the property used for conditional hiding [" + sourcePropertyValue.propertyType + "] is currently not supported");
+                    return true;
+            }
         }
     }
 }
