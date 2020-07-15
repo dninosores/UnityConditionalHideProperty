6a78dfb23a02da72a9bb8a6ce4e34623d5242750
Olly <dninosores@gmail.com>
2020-07-15 15:26:55 -0700

    Removed debug try catch

--- PropertyDrawerFinder.cs
+++ PropertyDrawerFinder.cs
@@ -40,100 +40,95 @@ namespace dninosores.UnityEditorAttributes
 			FieldInfo fi = parentType.GetField(fullPath[0]);
 			for (int i = 1; i < fullPath.Length; i++)
 			{
-				try
+				fi = fi.FieldType.GetField(fullPath[i]);
+				if (fi.FieldType.IsArray && i + 2 < fullPath.Length && fullPath[i + 1] == "Array")
 				{
-					fi = fi.FieldType.GetField(fullPath[i]);
-					if (fi.FieldType.IsArray && i + 2 < fullPath.Length && fullPath[i + 1] == "Array")
+					string dataString = fullPath[i + 2];
+					Regex pattern = new Regex(@"^data\[\d+\]$");
+					Match m = pattern.Match(dataString);
+					if (m.Success)
 					{
-						string dataString = fullPath[i + 2];
-						Regex pattern = new Regex(@"^data\[\d+\]$");
-						Match m = pattern.Match(dataString);
-						if (m.Success)
+						if (i + 2 == fullPath.Length - 1)
 						{
-							if (i + 2 == fullPath.Length - 1)
-							{
-								break;
-							}
-							else
-							{
-								Type arrayType = fi.FieldType.GetElementType();
-								fi = arrayType.GetField(fullPath[i + 3]);
-								i += 3;
-							}
-
+							break;
 						}
+						else
+						{
+							Type arrayType = fi.FieldType.GetElementType();
+							fi = arrayType.GetField(fullPath[i + 3]);
+							i += 3;
+						}
+
 					}
-				} catch (NullReferenceException n)
-				{
-					float a = 0;
 				}
 			}
-			return fi.FieldType;
-		}
+				return fi.FieldType;
+			}
 
 
-		/// <summary>
-		/// Returns custom property drawer for type if one could be found, or null if 
-		/// no custom property drawer could be found. Does not use cached values, so it's resource intensive.
-		/// </summary>
-		public static PropertyDrawer Find(Type propertyType)
-		{
-			foreach (Assembly assem in AppDomain.CurrentDomain.GetAssemblies())
+			/// <summary>
+			/// Returns custom property drawer for type if one could be found, or null if 
+			/// no custom property drawer could be found. Does not use cached values, so it's resource intensive.
+			/// </summary>
+			public static PropertyDrawer Find(Type propertyType)
 			{
-				foreach (Type candidate in assem.GetTypes())
+				foreach (Assembly assem in AppDomain.CurrentDomain.GetAssemblies())
 				{
-					FieldInfo typeField = typeof(CustomPropertyDrawer).GetField("m_Type", BindingFlags.NonPublic | BindingFlags.Instance);
-					FieldInfo childField = typeof(CustomPropertyDrawer).GetField("m_UseForChildren", BindingFlags.NonPublic | BindingFlags.Instance);
-					foreach (Attribute a in candidate.GetCustomAttributes(typeof(CustomPropertyDrawer)))
+					foreach (Type candidate in assem.GetTypes())
 					{
-						if (a.GetType().IsSubclassOf(typeof(CustomPropertyDrawer)) || a.GetType() == typeof(CustomPropertyDrawer))
+						FieldInfo typeField = typeof(CustomPropertyDrawer).GetField("m_Type", BindingFlags.NonPublic | BindingFlags.Instance);
+						FieldInfo childField = typeof(CustomPropertyDrawer).GetField("m_UseForChildren", BindingFlags.NonPublic | BindingFlags.Instance);
+						foreach (Attribute a in candidate.GetCustomAttributes(typeof(CustomPropertyDrawer)))
 						{
-							CustomPropertyDrawer drawerAttribute = (CustomPropertyDrawer)a;
-							Type drawerType =  (Type) typeField.GetValue(drawerAttribute);
-							if (drawerType == propertyType || 
-								((bool)childField.GetValue(drawerAttribute) && propertyType.IsSubclassOf(drawerType)) ||
-								((bool)childField.GetValue(drawerAttribute) && IsGenericSubclass(drawerType, propertyType)))
+							if (a.GetType().IsSubclassOf(typeof(CustomPropertyDrawer)) || a.GetType() == typeof(CustomPropertyDrawer))
 							{
-								if (candidate.IsSubclassOf(typeof(PropertyDrawer))){
-									return (PropertyDrawer)Activator.CreateInstance(candidate);
+								CustomPropertyDrawer drawerAttribute = (CustomPropertyDrawer)a;
+								Type drawerType = (Type)typeField.GetValue(drawerAttribute);
+								if (drawerType == propertyType ||
+									((bool)childField.GetValue(drawerAttribute) && propertyType.IsSubclassOf(drawerType)) ||
+									((bool)childField.GetValue(drawerAttribute) && IsGenericSubclass(drawerType, propertyType)))
+								{
+									if (candidate.IsSubclassOf(typeof(PropertyDrawer)))
+									{
+										return (PropertyDrawer)Activator.CreateInstance(candidate);
+									}
 								}
+
 							}
-							
 						}
+
+
 					}
-
-
 				}
+
+
+
+				return null;
 			}
 
-
-
-			return null;
-		}
-
-		/// <summary>
-		/// Returns true if the parent type is generic and the child type implements it.
-		/// </summary>
-		private static bool IsGenericSubclass(Type parent, Type child)
-		{
-			if (!parent.IsGenericType)
+			/// <summary>
+			/// Returns true if the parent type is generic and the child type implements it.
+			/// </summary>
+			private static bool IsGenericSubclass(Type parent, Type child)
 			{
-				return false;
-			}
-
-			Type currentType = child;
-			bool isAccessor = false;
-			while (!isAccessor && currentType != null)
-			{
-				if (currentType.IsGenericType && currentType.GetGenericTypeDefinition() == parent.GetGenericTypeDefinition())
+				if (!parent.IsGenericType)
 				{
-					isAccessor = true;
-					break;
+					return false;
 				}
-				currentType = currentType.BaseType;
-			}
-			return isAccessor;
-		}
 
+				Type currentType = child;
+				bool isAccessor = false;
+				while (!isAccessor && currentType != null)
+				{
+					if (currentType.IsGenericType && currentType.GetGenericTypeDefinition() == parent.GetGenericTypeDefinition())
+					{
+						isAccessor = true;
+						break;
+					}
+					currentType = currentType.BaseType;
+				}
+				return isAccessor;
+			}
+
+		}
 	}
-}
